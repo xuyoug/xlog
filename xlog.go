5410d4aef71bc39d35330aa57e66f1265eaeba12
徐友 <xuyoug@xuyougM.local>
2015-05-26 22:40:53 +0800

    20150526

--- xlog.go
+++ xlog.go
@@ -5,18 +5,18 @@ import (
 	"io"
 	"os"
 	"strings"
-	"sync"
 	"time"
 )
 
-// Curr version 1.1
-// author xuyou
+// Curr version 1.2
+// author xuyoug
 // mail:xuyoug@yeah.net
-// 2015-04-27
+// 2015-05-26
 //
 
 // 调用方式
-// l, err := xlog.NewXlog("log/log--{date}--{index}.log", 10000, "10M", xlog.TruncByday)
+// 依次传入：文件名、buf大小、log切换大小格式的字符串、和按时间切换的类型
+// l, err := xlog.NewXlog("log/log-{date}-{index}.log", 10000, "10M", xlog.TruncByday)
 // 	defer l.Close()
 // 	if err != nil {
 // 		fmt.Println(err)
@@ -25,10 +25,11 @@ import (
 // l.WriteString("test","test")
 // l.Witef("this is %d day",i)
 
-// 后期需要调整：（？自用不需要了）
-// 现在统一在log前加上了时间，精确到秒，后期是否需要自定义格式？是否需要参考Log？
+// 不定义日志级别（这个东西很坑，大家需求都不一样，请需要的朋友在外层自定义实现）
+// 统一在log每一行前加上了时间，精确到秒
 
 // version 1.1
+// 2015-04-27
 // 再次优化程序结构
 // 增加注释
 // ticker的优化
@@ -42,7 +43,7 @@ import (
 // 可以应用于生产环境
 // 2015-4-26
 // version 0.1
-// need to fix cpu占用的bug
+// fix cpu占用的bug
 // 结构的优化
 
 type TruncType string
@@ -56,10 +57,10 @@ const (
 
 const (
 	DefaultLogName string = "Log.log"
-	DefaultLogSize int64  = 500 * 1000 * 1000
+	DefaultLogSize int64  = 50 * 1000 * 1000
 )
 
-//获取当前文件大小
+//getFileSize获取文件大小
 func getFileSize(file *os.File) (size int64, err error) {
 	var tmpfileinfo os.FileInfo
 	tmpfileinfo, err = file.Stat()
@@ -69,7 +70,7 @@ func getFileSize(file *os.File) (size int64, err error) {
 	return tmpfileinfo.Size(), err
 }
 
-//根据设置的大小格式，判断切换大小
+//getFmtSize根据设置的大小格式，判断切换大小
 func getFmtSize(s string) (size int64, err error) {
 	st := strings.ToUpper(s)
 	if st == "" {
@@ -83,7 +84,7 @@ func getFmtSize(s string) (size int64, err error) {
 			_, err = fmt.Sscanf(st, "%d", &ti)
 		}
 		if err != nil {
-			fmt.Println("Error in format switch size:", err.Error())
+			panic("Error in format switch size")
 			return 0, err
 		}
 	}
@@ -91,11 +92,11 @@ func getFmtSize(s string) (size int64, err error) {
 		return 0, nil
 	}
 	switch ts {
-	case "K":
+	case "K", "KB":
 		return ti * 1000, nil
-	case "M":
+	case "M", "MB":
 		return ti * 1000 * 1000, nil
-	case "G":
+	case "G", "GB":
 		return ti * 1000 * 1000 * 1000, nil
 	case "B", "":
 		return ti, nil
@@ -113,13 +114,13 @@ func (x *Xlog) getBaseLog() (d string) {
 	}
 	switch x.Trunc {
 	case TruncByday:
-		x.truncname = time.Now().Format("2006-01-02")
+		x.truncname = time.Now().Format("20060102")
 	case TruncByhour:
-		x.truncname = time.Now().Format("2006-01-02_15")
+		x.truncname = time.Now().Format("2006010215")
 	case TruncBymin:
-		x.truncname = time.Now().Format("2006-01-02_1504")
+		x.truncname = time.Now().Format("200601021504")
 	case TruncByNo:
-		x.truncname = time.Now().Format("2006-01-02") //不按时间切换的话还是按启动日期记录
+		x.truncname = time.Now().Format("20060102") //不按时间切换的话则按启动日期记录
 	default:
 		x.truncname = ""
 	}
@@ -141,9 +142,10 @@ func (x *Xlog) getBaseLog() (d string) {
 }
 
 //进行log切换
-func (x *Xlog) switchlog() (is bool, err error) {
+func (x *Xlog) switchlog() (is bool) {
 	//判断是否需要根据大小切换
 	//对于自动到达相同log组最后一个的情况，如果需要判断大小仍然进行大小判断，不判断的话就继续写
+	var err error
 	tmpi1 := x.curIndex
 	if x.swcsize != 0 {
 		x.checkSize()
@@ -159,7 +161,7 @@ func (x *Xlog) switchlog() (is bool, err error) {
 	}
 	//如果都不需要则不切换log
 	if tmpi1 == tmpi2 || oldLogname == newLogname {
-		return false, nil
+		return false
 	}
 
 	// open new file fist
@@ -167,22 +169,19 @@ func (x *Xlog) switchlog() (is bool, err error) {
 	tmp_curFile, err = os.OpenFile(newLogname, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0777)
 	var oldcurFile *os.File
 	if err != nil {
-		fmt.Println("Error in switch log (open new file):", err.Error())
+		panic("Error in switch log (open new file)")
 	} else {
 		oldcurFile = x.curFile
 		// if there is no err then use new file
-		x.filemutex.Lock()
 		x.curFile = tmp_curFile
 		x.curLogname = newLogname
-		x.filemutex.Unlock()
 	}
 	// close old file then
 	err = oldcurFile.Close()
 	if err != nil {
-		fmt.Println("Error in switch log (close old file):", err.Error())
-		return true, err
+		panic("Error in switch log (close old file)")
 	}
-	return true, nil
+	return true
 }
 
 //检查文件大小是否达到切换标准，达到的话标记一下。不切换的话不标记
@@ -225,13 +224,12 @@ type Xlog struct {
 	Switchsize  string
 	swcsize     int64
 	logCurindex int
-	filemutex   *sync.Mutex
 	ticker      *time.Ticker
 	truncname   string
 }
 
 //初始化函数 依次传入：文件名、buf大小、log切换大小格式的字符串、和按时间切换的类型
-func NewXlog(s string, buf int, swcsizes string, tr TruncType) (*Xlog, error) {
+func NewXlog(s string, bufsize int, swcsizes string, tr TruncType) (*Xlog, error) {
 	x := new(Xlog)
 	var err error
 	x.Logname = s
@@ -240,32 +238,27 @@ func NewXlog(s string, buf int, swcsizes string, tr TruncType) (*Xlog, error) {
 	x.Switchsize = swcsizes
 	x.swcsize, _ = getFmtSize(swcsizes)
 	x.curIndex = 0
-	x.filemutex = new(sync.Mutex)
-	x.c = make(chan string, buf)
+	x.c = make(chan string, bufsize)
 	if x.swcsize != 0 {
-		//fmt.Println("go to the last log ,the switch size is:", x.swcsize)
 		x.gotoLastFile()
-		//fmt.Println("curr index:", x.curIndex)
 	}
 	x.curLogname = x.getBaseLog()
 	if x.swcsize != 0 || x.Trunc != TruncByNo {
-		x.ticker = time.NewTicker(time.Millisecond * 200)
+		x.ticker = time.NewTicker(time.Millisecond * 200) //每200ms进行一次切换检测
 	}
 	x.curFile, err = os.OpenFile(x.curLogname, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0777)
 	go x.pushlog()
 	return x, err
 }
 
-//关闭函数 关闭缓存chan，关闭ticker
+//Close关闭log对象：关闭缓存chan，关闭ticker
 //注意：要当所有的内容写完之后才会进行关闭
 func (x *Xlog) Close() (err error) {
-	x.filemutex.Lock()
 	for {
 		if x.GetBufDep() == 0 {
 			close(x.c)
 			err = x.curFile.Close()
 			x.ticker.Stop()
-			x.filemutex.Unlock()
 			return err
 		} else {
 			time.Sleep(time.Nanosecond * 10000)
@@ -286,9 +279,7 @@ func (x *Xlog) WriteString(s ...interface{}) {
 }
 
 func (x *Xlog) Log(s ...interface{}) {
-	tmps := fmt.Sprintln(s...)
-	tmps = time.Now().Format("2006-01-02 15:04:05 ") + tmps
-	x.c <- tmps
+	x.WriteString(s...)
 }
 
 func (x *Xlog) Writef(s string, i ...interface{}) {
@@ -299,17 +290,15 @@ func (x *Xlog) Writef(s string, i ...interface{}) {
 
 //作为独立gorotiune执行  负责获取缓存并写入文件
 //负责ticker的调度
-//实现无操作时的休眠 ？
+//实现无操作时的休眠
 func (x *Xlog) pushlog() {
 	var sleeptimes int
-	var err error
 	var issleep bool = false
 	for {
 		select {
 		case s, ok := <-x.c:
 			if issleep { //从休眠中唤醒需要先切换log、重新设置ticker
-				_, err = x.switchlog()
-				checkErr(err)
+				x.switchlog()
 				x.ticker = time.NewTicker(time.Millisecond * 200)
 			}
 			if ok {
@@ -322,8 +311,7 @@ func (x *Xlog) pushlog() {
 				x.ticker.Stop()
 				issleep = true
 			}
-			_, err = x.switchlog()
-			checkErr(err)
+			x.switchlog()
 			sleeptimes++
 		}
 	}
